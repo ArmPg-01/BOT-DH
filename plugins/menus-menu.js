697d65071f28b028ca1f11125f8d096224073787
Don Michifuz <Arm0899@outlook.es>
2025-04-23 16:35:58 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -33,7 +33,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `*¡HOLA! ${taguser}* 
-*BIENVENIDO AL MENU DE DON MICHIFUZ 🌷*
+*BIENVENIDO AL MENU DE BOT DH 🌷*
 
 *··················································*
 
